37017fef6fb503b190ab30fbc3076b67fbbe821b
ns-morozova <ns_morozova@mail.ru>
2025-11-21 02:40:14 +0300

    Second-level heading component

--- src/components/sections/home/TrendingCollection.vue
+++ src/components/sections/home/TrendingCollection.vue
@@ -1,34 +1,31 @@
 <template>
-    <section class="trending">
-      <h2>Trending Collection</h2>
-      <p>Checkout our weekly updated trending collection.</p>
-      
-    </section>
-  </template>
-  
-  <script setup lang="ts">
-  // import { RouterLink } from 'vue-router'
-  // import Button from '@/components/ui/Button.vue';
-  </script>
-  
-  <style scoped lang="scss">
-  @use '@/styles/mixins' as *;
-  
+  <section class="trending">
+    <HeadingH2
+      text="Trending Collection"
+      extra-class="trending__title"
+    />
+    <p class="trending__subtitle">Checkout our weekly updated trending collection.</p>
+  </section>
+</template>
+
+<script setup lang="ts">
+import HeadingH2 from '@/components/ui/HeadingH2.vue'
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/mixins' as *;
+
+.trending {
+  padding: 40px 0;
+}
+
+.trending__title {
+  margin-bottom: 10px;
+}
+
+@include respond-to(lg) {
   .trending {
-    padding: 40px 0;
+    padding: 80px 0;
   }
-  
-  @include respond-to(lg) {
-    .trending {
-      padding: 80px 0;
-    }
-  }
-  
-  // @include respond-to(xl) {
-  //   .hero__title {
-  //     font-size: 68px;
-  //     line-height: 1.1;
-  //   }
-  // }
-  </style>
-  
+}
+</style>
---
+++ src/components/ui/HeadingH2.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface UiHeadingH2Props {
+  text: string
+  extraClass?: string | string[]
+}
+
+const props = defineProps<UiHeadingH2Props>()
+</script>
+
+<template>
+  <h2
+    :class="[
+      'ui-heading',
+      'ui-heading--level-2',
+      props.extraClass
+    ]"
+  >
+    {{ props.text }}
+  </h2>
+</template>
+
+<style scoped lang="scss">
+@use '@/styles/mixins' as *;
+
+.ui-heading {
+  font-weight: 600;
+}
+
+.ui-heading--level-2 {
+  font-size: 28px;
+  line-height: 1.4;
+}
+
+@include respond-to(lg) {
+  .ui-heading--level-2 {
+    font-size: 38px;
+    line-height: 1.2;
+  }
+}
+</style>
