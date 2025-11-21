989fed9038c841b1c1b40db19011d74015e2d4db
ns-morozova <ns_morozova@mail.ru>
2025-11-21 21:52:51 +0300

    Fixed class names

--- src/components/cards/CardCollection.vue
+++ src/components/cards/CardCollection.vue
@@ -1,19 +1,19 @@
 <template>
     <article class="card">
-        <div class="card__image-main">
+        <div class="card__main-img">
             <img src="/images/cardCollectionMain1.png" alt="Main Image" />
         </div>
 
-        <div class="card__images">
+        <div class="card__thumbs">
             <img src="/images/cardCollectionExtra1_1.png" alt="Image" />
             <img src="/images/cardCollectionExtra1_2.png" alt="Image" />
-            <div>1025+</div>
+            <div class="card__more">1025+</div>
         </div>
 
         <div class="card__info">
             <h3>DSGN Animals</h3>
-            <div class="card__info-author">
-                <div class="img-wrapper">
+            <div class="card__author">
+                <div class="card__author-avatar">
                     <img src="/images/avatarDefault.png" alt="Avatar" />
                 </div>
                 <span>MrFox</span>
@@ -32,7 +32,7 @@
     gap: 15px;
 }
 
-.card__image-main {
+.card__main-img {
     border-radius: 20px;
     overflow: hidden;
 
@@ -43,7 +43,7 @@
     }
 }
 
-.card__images {
+.card__thumbs {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 15px;
@@ -52,7 +52,7 @@
         border-radius: 20px;
     }
 
-    div {
+    .card__more {
         display: flex;
         align-items: center;
         justify-content: center;
@@ -69,32 +69,30 @@
         margin-bottom: 10px;
     }
 
-    .card__info-author {
+    .card__author {
         display: flex;
         align-items: center;
         gap: 12px;
+    }
 
-        .img-wrapper {
-            width: 24px;
-            aspect-ratio: 1;
-            flex-shrink: 0;
-            border-radius: 50%;
-            overflow: hidden;
+    .card__author-avatar {
+        width: 24px;
+        aspect-ratio: 1;
+        flex-shrink: 0;
+        border-radius: 50%;
+        overflow: hidden;
 
-            img {
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-            }
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
         }
     }
 }
 
 @include respond-to(lg) {
-    .card__images {
-        div {
-            font-size: 22px;
-        }
+    .card__more {
+        font-size: 22px;
     }
 }
 </style>
