33a812e3f2d8b18156669676d224d13f9dd4b115
ns-morozova <ns_morozova@mail.ru>
2025-11-25 23:22:58 +0300

    Basic creators section

---
+++ src/components/sections/home/TopCreators.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="creators">
+      <div class="creators__heading">
+        <HeadingH2 text="Top creators" />
+        <Text value="Checkout Top Rated Creators on the NFT Marketplace" />
+      </div>
+    </section>
+  </template>
+  
+  <script setup lang="ts">
+  import HeadingH2 from '@/components/ui/HeadingH2.vue'
+  import Text from '@/components/ui/Text.vue'
+  </script>
+  
+  <style scoped lang="scss">
+  @use '@/styles/mixins' as *;
+  
+  .creators {
+    padding: 40px 0;
+  }
+  
+  .creators__heading {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-bottom: 40px;
+  }
+  
+  @include respond-to(md) {
+    .creators {
+      padding: 80px 0;
+    }
+  }
+  </style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <HeroSection />
   <TrendingCollection />
+  <TopCreators />
 </template>
 
 <script setup lang="ts">
 import HeroSection from '@/components/sections/home/HeroSection.vue'
 import TrendingCollection from '@/components/sections/home/TrendingCollection.vue'
+import TopCreators from '@/components/sections/home/TopCreators.vue'
 </script>
