4bca59e144157d00af9f68f6f0e74533cda08d92
ns-morozova <ns_morozova@mail.ru>
2025-11-25 23:32:37 +0300

    Cards for top creators

---
+++ src/components/cards/CardCreator.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+type CardCreatorProps = {
+  avatar: string
+  name: string
+  totalSales: number
+}
+
+defineProps<CardCreatorProps>()
+</script>
+
+<template>
+  <article class="card">
+    <div class="card__avatar">
+      <img :src="avatar" :alt="`${name} main image`" />
+    </div>
+
+    <div class="card__info">
+      <p class="card__info-name">{{ name }}</p>
+      <p class="card__info-total">{{ totalSales }}</p>
+    </div>
+  </article>
+</template>
+
+<style scoped lang="scss">
+@use '@/styles/mixins' as *;
+
+.card {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+
+.card__avatar {
+    width: 60px;
+    aspect-ratio: 1;
+    flex-shrink: 0;
+    border-radius: 50%;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+}
+
+.card__info {
+    .card__info-name {
+        font-size: 22px;
+        font-weight: 600;
+        margin-bottom: 5px;
+    }
+}
+
+@include respond-to(lg) {
+    
+}
+</style>
--- src/components/sections/home/TopCreators.vue
+++ src/components/sections/home/TopCreators.vue
@@ -4,12 +4,14 @@
         <HeadingH2 text="Top creators" />
         <Text value="Checkout Top Rated Creators on the NFT Marketplace" />
       </div>
+      <div class="creators__cards"></div>
     </section>
   </template>
   
   <script setup lang="ts">
   import HeadingH2 from '@/components/ui/HeadingH2.vue'
   import Text from '@/components/ui/Text.vue'
+//   import CardCreator from '@/components/cards/CardCreator.vue';
   </script>
   
   <style scoped lang="scss">
